fc54b331dcbdba708852719c689b2ebdcecd6ae8
erick show de bola <164902783+eerick66@users.noreply.github.com>
2024-11-25 14:44:11 +0000

    erick22

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é um vpn?',
+    ' estabelece uma conexão segura entre você e a Internet'
 )
 
 criaCartao(
     'Programação',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    'como o hacker pode invadir meu celular',
+    's ataques podem variar desde um e-mail de phishing até a interceptação de sua conexão em uma rede wi-fi não segura.'
 )
 
 criaCartao(
     'Programação',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    'como camuflar o ip?',
+    'so usar um vpn'
 )
 
 criaCartao(
     'Programação',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    'como combater o virus?',
+    'so fazer o uso de um antivirus.'
 )
 criaCartao(
     'Programação',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    'O que é malware?',
+    ' qualquer tipo de software malicioso projetado para prejudicar ou explorar qualquer dispositivo, serviço ou rede programável'
 )
 criaCartao(
     'Programação',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    'O que é virus?',
+    ' alterar a forma como um computador funciona e desenvolvido para se propagar de um computador para outro.'
 )
